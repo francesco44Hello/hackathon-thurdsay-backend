16bcd0e6b77e61bff1037b326597d17f75677457
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-03-30 15:23:22 +0100

    server listeneing

--- data/index.js
+++ data/index.js
@@ -1,13 +1,11 @@
-import pkg from 'pg';
+import pkg from "pg";
+
 const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_CONNECTION_URL,
+export const pool = new Pool({
+    connectionString: process.env.POSTGRESQL_CONNECTION_URL
 });
 
-module.exports = {
-    query: function (text, params) {
-        return pool.query(text, params);
-          },
-  pool : pool
-};
+export function query(text, params) {
+    return pool.query(text, params)
+}
